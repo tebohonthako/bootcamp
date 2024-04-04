65f567d469b6f6412916165ee86b80dc64d955ff
tebohonthako <tjaynthako@gmail.com>
2024-04-04 11:31:03 +0200

    switchcase operations

---
+++ javascript/switchcase.js
@@ -0,0 +1,38 @@
+// Simple calculator using javascript and switchcase
+
+// we need this library module to accept user input
+const prompt = require('prompt-sync')();
+let results;
+//accept the operator input
+const operator = prompt('enter operator (either +, -, * or /): ');
+
+//accept an operand input
+const num1 = parseFloat(prompt('Enter the first number: '));
+const num2 = parseFloat(prompt('Enter the second number: '));
+
+switch(operator){
+    //Formula for addition
+    case '+':
+    results = num1 + num2;
+    console.log(`${num1} + ${num2} = ${results}`);
+    break;
+
+    //Formula for subtraction
+    case '-':
+    results = num1 - num2;
+    console.log(`${num1} + ${num2} = ${results}`);
+    break;
+
+
+    //Formula for multiplication
+    case '*':
+    results = num1 * num2;
+    console.log(`${num1} + ${num2} = ${results}`);
+    break;
+
+    //Formula for division
+    case '/':
+    results = num1 / num2;
+    console.log(`${num1} + ${num2} = ${results}`);
+    break;
+}
