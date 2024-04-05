1c60d478c2f73f66b1673f83eadae1b9bb28ffd0
tebohonthako <tjaynthako@gmail.com>
2024-04-05 14:04:24 +0200

    functions

---
+++ javascript/checkfunction.js
@@ -0,0 +1,32 @@
+//let age;
+let prompt = require("prompt-sync")();
+
+//Lets write a program that will check if the user is old enough to access site.
+
+function checkAge(age){
+    if(age >= 18){
+        return true;
+    }else {
+        return console.log("You do not have permission");
+    }
+
+}
+
+let age = prompt("How old are you? ",18)
+if(checkAge(age)){
+    console.log("Access granted!")
+}else {
+    console.log("Access is denied")
+}
+
+
+function showMovie(){
+    let d = 1000;
+    let i = 2;
+    for(i = 100000; i>d; i--){
+        console.log("Access granted")
+        console.log("Playing a movie....")
+    }
+}
+showMovie();
+
